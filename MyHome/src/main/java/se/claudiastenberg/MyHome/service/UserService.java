17b0b1fea00e05a56c1da5aa3b405e3b3fc60824
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2018-08-03 11:49:24 +0200

    Utkast1FörKopplingTillRassberyPi

--- MyHome/src/main/java/se/claudiastenberg/MyHome/service/UserService.java
+++ MyHome/src/main/java/se/claudiastenberg/MyHome/service/UserService.java
@@ -30,31 +30,68 @@ public class UserService {
 
     public User getIdFromUser(Long id){
      Optional <User> userOptional = repository.findById(id);
-
-        try {
-            URL url = new URL("http://84.217.194.147:4000/post");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Content-Type", "application/json");
-            String contentType = con.getHeaderField("Content-Type");
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
-
-        int account = userOptional.get().getAccountName();
-        boolean status = userOptional.get().isStatus();
-        int message = account;
-
         if (userOptional.isPresent()){
 
+            /*Kolla lösenordet innan i databasen*/
+            // Sending post request
+            // HttpURLConnectionExample http = new HttpURLConnectionExample();
 
+            // int AnvändarID = id från databasen
+            //http.sendingPostRequest(AnvändarID);
 
-                return userOptional.get();
+            return userOptional.get();
             }
         throw new NotFoundException();
     }
 
+/**Här ska metod för post till rasberrypi ligga med en input från databasen
+ *
+ *
+ *  // om jag får ett post anrop
+ // ta lösenordet, kolla efter användarID i databasen
+ // kör metoden sendingPostRequest(användarID)
+ // HTTP Post request
+ private void sendingPostRequest(int AnvändarID) throws Exception {
 
+ String url = "http://84.217.194.147:4000/post";
+ URL obj = new URL(url);
+ HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+ // Setting basic post request
+ con.setRequestMethod("POST");
+ con.setRequestProperty("User-Agent", USER_AGENT);
+ con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+ con.setRequestProperty("Content-Type","application/json");
+
+ String postJsonData = String.Format("{\"password\":\"{0}\"}", AnvändarID);
+ System.out.println(postJsonData);
+
+ // Send post request
+ con.setDoOutput(true);
+ DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+ wr.writeBytes(postJsonData);
+ wr.flush();
+ wr.close();
+
+ int responseCode = con.getResponseCode();
+ System.out.println("nSending 'POST' request to URL : " + url);
+ System.out.println("Post Data : " + postJsonData);
+ System.out.println("Response Code : " + responseCode);
+
+ BufferedReader in = new BufferedReader(
+ new InputStreamReader(con.getInputStream()));
+ String output;
+ StringBuffer response = new StringBuffer();
+
+ while ((output = in.readLine()) != null) {
+ response.append(output);
+ }
+ in.close();
+
+ //printing result from response
+ System.out.println(response.toString());
+ }
+ * **/
 
 
 
